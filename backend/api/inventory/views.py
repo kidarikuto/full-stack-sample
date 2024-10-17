0c1e0fa4cb52167a131f340bd9e54a471013176a
kidarikuto <tan04tan20@gmail.com>
2024-10-18 00:07:14 +0900

    backend API POST maked

--- backend/api/inventory/views.py
+++ backend/api/inventory/views.py
@@ -2,20 +2,35 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.views import APIView
-from rest_framework.responese import Responese
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework import status
+
+from rest_framework.viewsets import ModelViewSet
 
 class ProductView(APIView):
     """
     商品操作に関する関数
     """
 
-    def get(self, requet format=None):
+    def get(self, request, format=None):
         """
         商品の一覧を取得する
         """
-        queryset = Product.object.all()
+        queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
+        
         return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        #validationを通らなかった場合、例外を投げる
+        serializer.is_valid(raise_exception=True)
+        #検証したデータを永続化する
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+class ProductModelViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
