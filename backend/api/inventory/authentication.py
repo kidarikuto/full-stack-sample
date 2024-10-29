aa5fab137d0d2fc56d2301130359abacde78f73e
kidarikuto <tan04tan20@gmail.com>
2024-10-30 06:26:17 +0900

    LogoutView,RetryViewの実装　JWTをクッキーに保存して自動認証しlogoutページでpostするとクッキーの削除される。access tokenの期限が切れたとはRetryページでpostすると復元できるようになった

--- backend/api/inventory/authentication.py
+++ backend/api/inventory/authentication.py
@@ -3,12 +3,22 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 class AccessJWTAuthentication(JWTAuthentication):
     def get_header(self, request):
         token = request.COOKIES.get('access')
-        request.META['HTTP_AUTHORIZATION'] = '{header_type}{access_token}'.format(
-            header_type="Bearer",access_token=token)
-        return super().get_header(request)
-    
+        """本ver"""
+        # request.META['HTTP_AUTHORIZATION'] = '{header_type}{access_token}'.format(
+        #     header_type="Bearer",access_token=token)
+        # return super().get_header(request)
+        """chatGPTver"""
+        if token is None:
+            return None  # トークンが存在しない場合、Noneを返す
+        
+        # ヘッダー形式を設定
+        return f"Bearer {token}".encode("utf-8")
+
+
 class RefreshJWTAuthentication(JWTAuthentication):
     def get_header(self, request):
         refresh = request.COOKIES.get('refresh')
         request.META['HTTP_REFRESH_TOKEN'] = refresh
         return super().get_header(request)
+
+        
--- backend/api/inventory/views.py
+++ backend/api/inventory/views.py
@@ -17,6 +17,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
 from api.inventory.authentication import RefreshJWTAuthentication
+from .authentication import AccessJWTAuthentication, RefreshJWTAuthentication
 
 
 
@@ -24,8 +25,8 @@ class ProductView(APIView):
     """
     商品操作に関する関数
     """
-    authentication_classes = [JWTAuthentication]
-    permission_classes = []
+    authentication_classes = [AccessJWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_object(self, pk):
         try:
@@ -121,12 +122,7 @@ class LoginView(APIView):
     permission_classes = []
 
     def post(self, request):
-        # return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        """
-        json形式のデータをシリアライズする
-
-        """
-        serializer = TokenObtainPairSerializer(data=request.data) 
+        serializer = TokenObtainPairSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         access = serializer.validated_data.get("access", None)
         refresh = serializer.validated_data.get("refresh", None)
@@ -139,4 +135,30 @@ class LoginView(APIView):
             return response
         return Response({'errMsg': 'ユーザーの認証に失敗しました'}, status=status.HTTP_401_UNAUTHORIZED)
     
-    
+class RetryView(APIView):
+    authentication_classes = [RefreshJWTAuthentication]
+    permission_classes = []
+
+    def post(self, request):
+        request.data['refresh'] = request.META.get('HTTP_REFRESH_TOKEN')
+        serializer = TokenRefreshSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        access = serializer.validated_data.get('access', None)
+        refresh = serializer.validated_data.get("refresh", None)
+        if access:
+            response = Response(status=status.HTTP_200_OK)
+            max_age = settings.COOKIE_TIME
+            response.set_cookie('access', access, httponly=True, max_age=max_age)
+            response.set_cookie('refresh', refresh, httponly=True, max_age=max_age)
+            return response
+        return Response({'errMsg': 'ユーザーの認証に失敗しました'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request, *args):
+        response = Response(status=status.HTTP_200_OK)
+        response.delete_cookie('access')
+        response.delete_cookie('refresh')
+        return response
